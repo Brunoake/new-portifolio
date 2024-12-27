2cdbc40e9aa61493512a4cc4e7c5bed99d6011e1
Brunoake <126792752+Brunoake@users.noreply.github.com>
2024-12-27 12:34:51 -0300

    .

---
+++ new-portifolio/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <nav>
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
